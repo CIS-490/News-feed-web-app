c56eb411dcf116770fa907f13ecb5c5bfcc1ae03
med256 <90477718+med256@users.noreply.github.com>
2021-10-07 16:34:19 -0400

    Add files via upload

--- components/Login.vue
+++ components/Login.vue
@@ -5,10 +5,13 @@
         <v-row align="center" justify="center" dense>
           <v-col cols="12" sm="8" md="4" lg="4">
             <v-card elevation="0">
+            <div class="text-center">
+                <h1 class="mb-2">Login</h1>
+              </div>
               <v-card-text>
                 <v-form>
                   <v-text-field label="Enter your email" name="email" prepend-inner-icon="mdi-mail" type="email" class="rounded-0" outlined></v-text-field>
-                  <v-text-field label="Enter your password" name="password" prepend-inner-icon="mdi-lock" type="password" suffix="| Forgot?" class="rounded-0" outlined></v-text-field>
+                  <v-text-field label="Enter your password" name="password" prepend-inner-icon="mdi-lock"  class="rounded-0" outlined> </v-text-field>
                   <v-btn class="rounded-0" color="#000000" x-large block dark>Login</v-btn>
                   <v-card-actions class="text--secondary">
                     <v-spacer></v-spacer>
