c9aa02d0508fe35a63191473025fb62a0bdb72d2
med256 <90477718+med256@users.noreply.github.com>
2021-10-07 16:12:51 -0400

    Add files via upload

--- App.vue
+++ App.vue
@@ -5,11 +5,14 @@
         <v-tabs>
           <v-tab @click="login = true">Login</v-tab>
           <v-tab @click="register = true; login = false">Register</v-tab>
+          <v-tab @click="forgotpassword = true; login = false; register = false">ForgotPassword</v-tab>
         </v-tabs>
         <div>
           <Login v-if="login" />
 
           <Register v-if="register" />
+
+          <ForgotPassword v-if="forgotpassword" />
         </div>
       </v-container>
     </v-main>
@@ -19,18 +22,21 @@
 <script>
   import Login from "@/components/Login";
   import Register from "@/components/Register";
+  import ForgotPassword from "@/components/ForgotPassword";
 
   export default {
     name: 'App',
     components: {
       Login,
-      Register
+      Register,
+      ForgotPassword
     },
 
     data(){
       return{
         login: true,
         register: false,
+        forgotpassword: false,
       }
     },
     
